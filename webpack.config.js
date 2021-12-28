7a9040ae1ac9eec13cbfdfc8b924ca46d5652821
你们的小f <nmdfzf404@163.com>
2021-12-28 10:14:02 +0800

    🔥 完成

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path')
+
+module.exports = {
+  entry: './stegcloak.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, '.'),
+    library:'StegCloak'
+  },
+}
