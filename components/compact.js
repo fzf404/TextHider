c1f8c94a4772dcb81e60370da19b809e735b6e8d
你们的小f <nmdfzf404@163.com>
2022-10-04 20:02:13 +0800

    :bookmark: v1.1.0

--- components/compact.js
+++ components/compact.js
@@ -1,70 +1,67 @@
-"use strict";
+'use strict'
 
-const { pipe, curry, sort, difference, __ } = require("ramda");
+const { pipe, curry, sort, difference, __ } = require('ramda')
 
-const { recursiveReplace } = require("./util");
+const { recursiveReplace } = require('./util')
 
-const lzutf8 = require("lzutf8");
+const lzutf8 = require('lzutf8')
 
 const compress = (x) =>
   lzutf8.compress(x, {
-    outputEncoding: "Buffer",
-  });
+    outputEncoding: 'Buffer',
+  })
 
 // Curried decompress
 
 const _lzutf8Decompress = curry(lzutf8.decompress)(__, {
-  inputEncoding: "Buffer",
-  outputEncoding: "String",
-});
+  inputEncoding: 'Buffer',
+  outputEncoding: 'String',
+})
 
 // Decompress a buffer using LZ decompression
-const decompress = pipe(Buffer.from, _lzutf8Decompress);
+const decompress = pipe(Buffer.from, _lzutf8Decompress)
 
 // Builds a ranking table and filters the two characters that can be compressed that yield good results
 
 const findOptimal = (secret, characters) => {
   const dict = characters.reduce((acc, data) => {
-    acc[data] = {};
-    return acc;
-  }, {});
-  const size = secret.length;
+    acc[data] = {}
+    return acc
+  }, {})
+  const size = secret.length
   for (let j = 0; j < size; j++) {
-    let count = 1;
+    let count = 1
     while (j < size && secret[j] === secret[j + 1]) {
-      count++;
-      j++;
+      count++
+      j++
     }
     if (count >= 2) {
-      let itr = count;
+      let itr = count
       while (itr >= 2) {
-        dict[secret[j]][itr] =
-          (dict[secret[j]][itr] || 0) + Math.floor(count / itr) * (itr - 1);
-        itr--;
+        dict[secret[j]][itr] = (dict[secret[j]][itr] || 0) + Math.floor(count / itr) * (itr - 1)
+        itr--
       }
     }
   }
-  const getOptimal = [];
+  const getOptimal = []
   for (const key in dict) {
     for (const count in dict[key]) {
-      getOptimal.push([key + count, dict[key][count]]);
+      getOptimal.push([key + count, dict[key][count]])
     }
   }
-  const rankedTable = sort((a, b) => b[1] - a[1], getOptimal);
+  const rankedTable = sort((a, b) => b[1] - a[1], getOptimal)
 
   let reqZwc = rankedTable
-    .filter((val) => val[0][1] === "2")
+    .filter((val) => val[0][1] === '2')
     .slice(0, 2)
-    .map((chars) => chars[0][0]);
+    .map((chars) => chars[0][0])
 
   if (reqZwc.length !== 2) {
-    reqZwc = reqZwc.concat(
-      difference(characters, reqZwc).slice(0, 2 - reqZwc.length)
-    );
+    reqZwc = reqZwc.concat(difference(characters, reqZwc).slice(0, 2 - reqZwc.length))
   }
 
-  return reqZwc.slice().sort();
-};
+  return reqZwc.slice().sort()
+}
 
 const zwcHuffMan = (zwc) => {
   const tableMap = [
@@ -74,15 +71,14 @@ const zwcHuffMan = (zwc) => {
     zwc[1] + zwc[2],
     zwc[1] + zwc[3],
     zwc[2] + zwc[3],
-  ];
+  ]
 
-  const _getCompressFlag = (zwc1, zwc2) =>
-    zwc[tableMap.indexOf(zwc1 + zwc2)]; // zwA,zwB => zwD
+  const _getCompressFlag = (zwc1, zwc2) => zwc[tableMap.indexOf(zwc1 + zwc2)] // zwA,zwB => zwD
 
-  const _extractCompressFlag = (zwc1) => tableMap[zwc.indexOf(zwc1)].split(""); // zwcD => zwA,zwcB
+  const _extractCompressFlag = (zwc1) => tableMap[zwc.indexOf(zwc1)].split('') // zwcD => zwA,zwcB
 
   const shrink = (secret) => {
-    const repeatChars = findOptimal(secret, zwc.slice(0, 4));
+    const repeatChars = findOptimal(secret, zwc.slice(0, 4))
     return (
       _getCompressFlag(...repeatChars) +
       recursiveReplace(
@@ -90,28 +86,28 @@ const zwcHuffMan = (zwc) => {
         repeatChars.map((x) => x + x),
         [zwc[4], zwc[5]]
       )
-    );
-  };
+    )
+  }
 
   const expand = (secret) => {
-    const flag = secret[0];
-    const invisibleStream = secret.slice(1);
-    const repeatChars = _extractCompressFlag(flag);
+    const flag = secret[0]
+    const invisibleStream = secret.slice(1)
+    const repeatChars = _extractCompressFlag(flag)
     return recursiveReplace(
       invisibleStream,
       [zwc[4], zwc[5]],
       repeatChars.map((x) => x + x)
-    );
-  };
+    )
+  }
 
   return {
     shrink,
     expand,
-  };
-};
+  }
+}
 
 module.exports = {
   compress,
   decompress,
   zwcHuffMan,
-};
+}
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, '.'),
-    library:'StegCloak'
+    library: 'StegCloak',
   },
+  mode: 'production',
 }
